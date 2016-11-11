829074b06cde0393519fecf8575a8f18f00ba53c
Eric Brelsford <ebrelsford@gmail.com>
2016-11-11 18:53:27 -0500

    GeoJSON file path

--- maprouletteupload/cmd.py
+++ maprouletteupload/cmd.py
@@ -11,20 +11,20 @@ from .upload import upload_tasks
 @click.option('--api-key', help='Your API key, via Maproulette.')
 @click.option('--challenge-id', type=int,
         help='The challenge ID these tasks should be added to.')
-@click.option('--geojson-file',
-        help=('A GeoJSON file of tasks to upload. Alternatively, you can '
-            'provide this file via stdin.'))
 @click.option('--identifier', type=str, default='identifier',
         help=('The name of the property to use as the identifier. Default: '
-              '"identifier".'))
+            '"identifier".'))
 @click.option('--instruction', type=str, default='instruction',
         help=('The name of the property to use as the instruction. Default: '
-              '"instruction".'))
+            '"instruction".'))
 @click.option('--name', type=str, default='name',
         help=('The name of the property to use as the name. Default: "name".'))
+@click.option('--geojson-file', type=click.Path(exists=True),
+        help=('A GeoJSON file of tasks to upload. Alternatively, you can '
+            'provide this file via stdin.'))
 @click.version_option()
-def upload(api_key, challenge_id, geojson_file, identifier, instruction, name):
-    """Upload a GeoJSON file of tasks to Maproulette."""
+def upload(api_key, challenge_id, identifier, instruction, name, geojson_file):
+    """Upload a GeoJSON file (or stdin) of tasks to Maproulette."""
     if not api_key:
         raise click.BadParameter('API key required. Please try again.')
     if not challenge_id:
@@ -40,7 +40,7 @@ def upload(api_key, challenge_id, geojson_file, identifier, instruction, name):
         'name_field': name,
     }
 
-    tasks = list(create_tasks(geojson), **create_tasks_kwargs)
+    tasks = list(create_tasks(geojson, **create_tasks_kwargs))
     response = upload_tasks(api_key, challenge_id, tasks)
     click.echo('Done uploading tasks. Status code: %d' % response.status_code)
     click.echo(response.text)
