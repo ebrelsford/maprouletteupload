399c05f8f4afd8f45d7ec331a4fcf5a4437ec457
Eric Brelsford <ebrelsford@gmail.com>
2016-11-11 19:32:46 -0500

    PEP8

--- maprouletteupload/tasks.py
+++ maprouletteupload/tasks.py
@@ -1,7 +1,8 @@
 import geojson
 
 
-def create_tasks(featurecollection, identifier_field='identifier', instruction_field='instruction', name_field='name'):
+def create_tasks(featurecollection, identifier_field='identifier',
+        instruction_field='instruction', name_field='name'):
 
     for feature in featurecollection['features']:
         identifier = feature['properties'][identifier_field]
