fbd5dea024b71145657bda3c35472700082dad18
Eric Brelsford <ebrelsford@gmail.com>
2016-11-14 13:54:32 -0500

    TODO done

--- maprouletteupload/cmd.py
+++ maprouletteupload/cmd.py
@@ -5,7 +5,6 @@ import webbrowser
 from .api import DIFFICULTIES, create_challenge, get_challenge, upload_tasks
 from .tasks import create_tasks
 
-# TODO use api to optionally create challenge too
 # TODO use api to figure out which tasks already exist
 
 @click.command()
